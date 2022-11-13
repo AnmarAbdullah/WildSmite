63d38f82a1add0b72ceef2f527e3dfcb6b95f03f
Dylan Suaris <dylansuaris@gmail.com>
2022-11-13 22:02:10 +0400

    Added Enemy Spawning to Level Generation!
    Dynamic in terms of adding A higher/Lower enemy count and changing the spawn radius size!

--- Assets/Dylan/Level Generation/Scripts/Spawning/SpawnEnemy.cs
+++ Assets/Dylan/Level Generation/Scripts/Spawning/SpawnEnemy.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
-    public Transform[] spawnSpots;
-    public GameObject enemyPrefab;
-    private int randSpawnSpots;
+    public GameObject[] enemyPrefab;
+
+    public float EnemyCount;
+    public float radius = 1;
 
     void Start()
     {
+        SpawnEnemies();
+    }
 
-        randSpawnSpots = Random.Range(0, spawnSpots.Length);
-        Instantiate(enemyPrefab, spawnSpots[randSpawnSpots].transform);
+    void SpawnEnemies()
+    {
+        
 
+        for (int i = 0; i < EnemyCount; i++)
+        {
+            Vector3 randPos = transform.position + Random.insideUnitSphere * radius;
+
+            int j = Random.Range(0, enemyPrefab.Length);
+
+            Instantiate(enemyPrefab[j], randPos, Quaternion.identity);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(this.transform.position, radius);
     }
 }
